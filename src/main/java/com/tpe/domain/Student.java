5d4111cee07534ef9b6542e8cf104e1c94a1a7ad
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2024-11-14 13:28:21 +0200

    setting the dependancies and application components

--- src/main/java/com/tpe/domain/Student.java
+++ src/main/java/com/tpe/domain/Student.java
@@ -1,4 +1,18 @@
 package com.tpe.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tbl_student")
 public class Student {
+
+    private Long id;
+    private String name;
+    private String lastName;
+    private Integer grade;
+    private String phoneNumber;
+    private LocalDateTime createdDate = LocalDateTime.now();
+
 }
