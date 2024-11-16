896d5380849d19b05ee68bc70f853172fb9c9b36
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2024-11-16 16:17:11 +0200

    setting the dependancies and application components

--- src/main/java/com/tpe/service/StudentService.java
+++ src/main/java/com/tpe/service/StudentService.java
@@ -2,6 +2,7 @@ package com.tpe.service;
 
 import com.tpe.domain.Student;
 import com.tpe.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,10 +10,11 @@ import java.util.List;
 @Service
 public class StudentService {
 
+    @Autowired
     private StudentRepository repository;
-    
+
     public List<Student> getAllStudents() {
 
-
+        return repository.findAll();
     }
 }
