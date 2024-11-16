41d53a2ef6083e7d8522453b3751bb6d61f52f67
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2024-11-16 13:54:02 +0200

    setting the dependancies and application components, declaring StudentController class

---
+++ src/main/java/com/tpe/controller/StudentController.java
@@ -0,0 +1,12 @@
+package com.tpe.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/students")    // http://localhost:8080/students
+public class StudentController {
+
+
+
+}
--- src/main/java/com/tpe/repository/StudentRepository.java
+++ src/main/java/com/tpe/repository/StudentRepository.java
@@ -2,6 +2,8 @@ package com.tpe.repository;
 
 import com.tpe.domain.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository // optional, because we're extending this interface from JpaRepository
 public interface StudentRepository extends JpaRepository<Student, Long> {
 }
